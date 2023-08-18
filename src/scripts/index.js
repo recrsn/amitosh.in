52552d1396d020d3663aa61a447f72c066919ca7
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2023-08-18 09:32:34 +0530

    Make fluid progressbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,23 +9,26 @@
             request.addEventListener('readystatechange', e => {
                 switch (request.readyState) {
                     case 1:
-                        progress.style.width = '25%';
+                        progress.style.transform = 'scaleX(.25)';
                         break;
                     case 2:
-                        progress.style.width = '50%';
+                        progress.style.transform = 'scaleX(.5)'
                         break;
                     case 3:
-                        progress.style.width = '75%';
+                        progress.style.transform = 'scaleX(.75)'
                         break;
                     case 4:
                         if (request.status === 200) {
-                            progress.style.width = '100%';
+                            progress.style.transform = 'scaleX(1)';
                             let responseDocument = new DOMParser().parseFromString(request.responseText, 'text/html');
                             target.innerHTML = responseDocument.querySelector(a.dataset.swap).innerHTML;
                             document.title = responseDocument.title;
 
                             history.pushState({}, '', a.href);
-                            progress.style.width = '0%';
+
+                            setTimeout(() => {
+                                progress.style.transform = 'scaleX(0)';
+                            }, 500);
                         } else {
                             target.innerHTML = `<h1>Error ${request.status}</h1>`;
                         }
@@ -39,7 +42,7 @@
             })
             request.addEventListener('progress', e => {
                 if (e.lengthComputable) {
-                    progress.style.width = `${75 + ((e.loaded / e.total) * 25)}%`;
+                    progress.style.transform = `scaleX(${0.75 + ((e.loaded / e.total) * 0.25)})`;
                 }
             });
             request.open('GET', a.href);
